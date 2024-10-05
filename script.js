3fda13ddae3254b744daea11628a20abc1531328
anusb412 <anusb412@gmail.com>
2024-10-05 19:45:49 -0400

    CLOCK - Displaying wrong day? lol

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-let count = 0;
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-document.getElementById("increase").addEventListener("click", increase());
+//d
 
-if(count < 0){
-    document.getElementById("count").style.color = "red";
-}
+const d = new Date();
+const day = days[d.getDay()];
+const hours = d.getHours();
+const amPm = hours >= 12 ? "PM" : "AM";
 
-function increase(){
-count++;
-document.getElementById("count").textContent = count;
-}
+document.getElementById("day").textContent = day.slice(0, 3);
+document.getElementById("hour").textContent = d.toLocaleTimeString('it-IT', {hour : 'numeric', hour12 : true}).replace(/AM|PM/g, "").trim();
+document.getElementById("minute").textContent = d.getMinutes();
+document.getElementById("ampm").textContent = amPm;
 
-function decrease() {
-count--;
-document.getElementById("count").textContent = count;
-}
+
+
+/*
+
+
+
+
+*/
+
 
 
 
